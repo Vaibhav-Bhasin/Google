848e75d73ed00d0ce1d073ac560b9c4d999f5009
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-16 22:32:18 +0530

    Update Google.py

--- Google.py
+++ Google.py
@@ -1,124 +1,65 @@
 import turtle
-
 gg turtle.Turtle()
-
 turtle.bgcolor("black") gg.color("#4285F4","#4285F4")
-
 gg.pensize(5)
-
 #gg.speed(0)
-
 # [google color codes ]
-
-#
-
-red
-
-= #DB4437
-
+#red  = #DB447
 # blue = #4285F4
-
 # green =#0F9D58
-
 # yellow =#F4B40
+
 def
-
 google():
-
 gg.forward(120)
-
 99.right(90)
-
 gg.circle(-150,50)
-
 gg.color("#8F9058") gg.circle(-150, 100)
-
 99.color("#F40408")
-
 99.circle(-150,60)
-
 gg.color("#DB4437","#084437")
-
 gg.begin_fill() gg.circle(-150, 100)
-
 99.right(90)
-
 gg.forward(50)
-
 99.right(90)
-
 99.circle(180,100)
-
 cat rma
-
 99.right(90)
-
 gg. forward(50)
-
 99.end_fill() gg.begin_fill()
-
 99.color("#F4B400","#F4B480")
-
 99.right(180) gg. forward(50)
 gg.right(90) gg.circle(100,60)
-
 gg.right(90)
-
 gg.forward(50)
-
 gg.right(90)
-
 gg.circle(-150,60)
-
 gg.end_fill()
-
 gg.right(90)
-
 gg. forward (50) gg.right(90)
-
 99.circle(100,60)
-
 gg.color("#0F9D58","#0F9D58") gg.begin_fill()
-
 gg.circle(100, 100)
-
 gg.right (90)
-
 gg.forward(50)
-
 99.right(90)
-
 gg.circle(-150,100) 99.right(90)
 gg. forward(50)
-
 gg.end_fill()
-
 99.right(90)
-
 09.circle(100,100)
-
 99.color("#4285F4","#4285F4") gg.begin fill()
-
 gg.circle(100,25)
-
 99. left(115)
-
 99. forward(65)
-
 99.right(90)
-
 99. forward(42)
-
 99.right(90) gg. forward(124)
-
 99.right(90)
-
 99.circle(-150,50) gg.right(90)
-
 99.forward(50) gg.end_fill()
-
 google()
-
 gg.hideturtle()
+
 
 turtle.done()
