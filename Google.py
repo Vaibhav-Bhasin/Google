4fc8348773f07aeeddd4e93da5d17b9457212850
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-16 22:27:35 +0530

    Update Google.py

--- Google.py
+++ Google.py
@@ -1,1 +1,23 @@
+import turtle
 
+gg turtle.Turtle()
+
+turtle.bgcolor("black") gg.color("#4285F4","#4285F4")
+
+gg.pensize(5)
+
+#gg.speed(0)
+
+# [google color codes ]
+
+#
+
+red
+
+= #DB4437
+
+# blue = #4285F4
+
+# green =#0F9D58
+
+# yellow =#F4B40
